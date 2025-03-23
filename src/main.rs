fe68afe3573e5f29e1077a34e5225e7a04417b9c
iliabox <iliabox@users.noreply.github.com>
2025-03-23 20:22:15 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,24 @@ use actix_web::{get, App, HttpServer, HttpRequest, Responder};
 use std::env;
 
 #[get("/")]
-async fn get_ip(req: HttpRequest) -> impl Responder {
-    if let Some(peer_addr) = req.peer_addr() {
-        format!("IP: {}", peer_addr.ip())
-    } else {
-        "Cannot determine IP".to_string()
+async fn get_headers(req: HttpRequest) -> impl Responder {
+    let mut headers_info = String::new();
+
+    for (header_name, header_value) in req.headers().iter() {
+        if let Ok(name) = header_name.to_str() {
+            if let Ok(value) = header_value.to_str() {
+                headers_info.push_str(&format!("{}: {}\n", name, value));
+            }
+        }
     }
+
+    if let Some(peer_addr) = req.peer_addr() {
+        headers_info.push_str(&format!("Peer IP: {}\n", peer_addr.ip()));
+    } else {
+        headers_info.push_str("Cannot determine Peer IP\n");
+    }
+
+    headers_info
 }
 
 #[actix_web::main]
@@ -17,7 +29,7 @@ async fn main() -> std::io::Result<()> {
         .parse()
         .expect("PORT must be a valid number");
 
-    HttpServer::new(|| App::new().service(get_ip))
+    HttpServer::new(|| App::new().service(get_headers))
         .bind(("0.0.0.0", port))?
         .run()
         .await
